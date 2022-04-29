228c810f51c0c6e0f6a05ac54e27fa718fee9ec6
harshitha r <57220633+harshithacastle@users.noreply.github.com>
2022-04-29 05:51:43 +0530

    Update main.go

--- main.go
+++ main.go
@@ -245,7 +245,7 @@ func GetTaxiTrips(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=1000"
+	var url = "https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=500"
 
 	res, err := http.Get(url)
 	if err != nil {
@@ -431,7 +431,7 @@ func GetUnemploymentRates(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/iqnk-2tcu.json?$limit=1000"
+	var url = "https://data.cityofchicago.org/resource/iqnk-2tcu.json?$limit=500"
 
 	res, err := http.Get(url)
 	if err != nil {
@@ -747,7 +747,7 @@ func GetBuildingPermits(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/building-permits.json?$limit=1500"
+	var url = "https://data.cityofchicago.org/resource/building-permits.json?$limit=500"
 
 	res, err := http.Get(url)
 	if err != nil {
